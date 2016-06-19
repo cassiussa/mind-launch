67771be6127ec27eb65a513b9f4859c274391f87
Cassius John-Adams <cassius.s.adams@gmail.com>
2016-06-19 13:47:45 -0400

    Began adding States and the Global variables

---
+++ Assets/Scripts/LayerMotion.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using System.Collections;
+using GlobalVariables;
+
+public class LayerMotion : MonoBehaviour {
+
+	public enum State { None, Initialize, First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eighth, Nineth, Tenth }
+	public State motionState = State.None;
+	State _prevMotionState = State.None;
+	public State _cacheMotionState = State.None;
+
+	public global.State level;	// Test to see if I can globalize an enum - looks like I can.  Note, it's static here
+
+
+	void Awake () {
+		// Set the initial state to noSpeed so that it can gather the details about game state and apply them to this layer's motion
+		SetState (State.Initialize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator Start() {
+		while (true) {	// Not sure this is going to work
+			switch (motionState) {
+			case State.None:
+				break;
+			case State.Initialize:
+				Initialize ();
+				break;
+			case State.First:
+				First ();
+				break;
+			case State.Second:
+				Second ();
+				break;
+			case State.Third:
+				Third ();
+				break;
+			case State.Fourth:
+				Fourth ();
+				break;
+			case State.Fifth:
+				Fifth ();
+				break;
+			case State.Sixth:
+				Sixth ();
+				break;
+			case State.Seventh:
+				Seventh ();
+				break;
+			case State.Eighth:
+				Eighth ();
+				break;
+			case State.Nineth:
+				Nineth ();
+				break;
+			case State.Tenth:
+				Tenth ();
+				break;
+			}
+			yield return null;
+		}
+	}
+	
+
+	void Initialize() {
+		if (_cacheMotionState != motionState) {
+			if (global.speed == 0)
+				global.speed = 1;
+			print (global.speed);
+			_cacheMotionState = motionState;
+		}
+	}
+
+	void First() {
+		if (_cacheMotionState != motionState) {
+
+			_cacheMotionState = motionState;
+		}
+	}
+
+	void Second() {
+		if (_cacheMotionState != motionState) {
+			
+			_cacheMotionState = motionState;
+		}
+	}
+
+	void Third() {
+		if (_cacheMotionState != motionState) {
+			
+			_cacheMotionState = motionState;
+		}
+	}
+
+	void Fourth() {
+		if (_cacheMotionState != motionState) {
+			
+			_cacheMotionState = motionState;
+		}
+	}
+
+	void Fifth() {
+		if (_cacheMotionState != motionState) {
+			
+			_cacheMotionState = motionState;
+		}
+	}
+
+	void Sixth() {
+		if (_cacheMotionState != motionState) {
+			
+			_cacheMotionState = motionState;
+		}
+	}
+
+	void Seventh() {
+		if (_cacheMotionState != motionState) {
+			
+			_cacheMotionState = motionState;
+		}
+	}
+
+	void Eighth() {
+		if (_cacheMotionState != motionState) {
+			
+			_cacheMotionState = motionState;
+		}
+	}
+
+	void Nineth() {
+		if (_cacheMotionState != motionState) {
+			
+			_cacheMotionState = motionState;
+		}
+	}
+
+	void Tenth() {
+		if (_cacheMotionState != motionState) {
+			
+			_cacheMotionState = motionState;
+		}
+	}
+
+
+	// Received by a Broadcast call
+	void NextSpeed() {
+		if (motionState == State.None)
+			SetState (State.First);
+		else if (motionState == State.First)
+			SetState (State.Second);
+		else if (motionState == State.Second)
+			SetState (State.Third);
+		else if (motionState == State.Third)
+			SetState (State.Fourth);
+		else if (motionState == State.Fourth)
+			SetState (State.Fifth);
+		else if (motionState == State.Fifth)
+			SetState (State.Sixth);
+		else if (motionState == State.Sixth)
+			SetState (State.Seventh);
+		else if (motionState == State.Seventh)
+			SetState (State.Eighth);
+		else if (motionState == State.Eighth)
+			SetState (State.Nineth);
+		else if (motionState == State.Nineth)
+			SetState (State.Tenth);
+		else
+			Debug.LogError ("An invalid state has been reached in the LayerMotion script", gameObject);
+	}
+
+	void SetState(State newState) {
+		_prevMotionState = motionState;
+		_cacheMotionState = State.None;
+		motionState = newState;
+	}
+}
---
+++ Assets/Scripts/LayerStates.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+using GlobalVariables;
+
+public class LayerStates : MonoBehaviour {
+
+	public enum State { None, Initialize, Upcoming, Current, Fading, Success, Fail, Passed }
+	public State layerState = State.None;
+	State _prevLayerState = State.None;
+	public State _cacheLayerState = State.None;
+
+	/*
+	#region Basic Getters/Setters
+	public State CurrentState { get { return layerState; } }
+	public State PrevState { get { return _prevLayerState; } }
+	#endregion
+	*/
+	
+	void Awake() {
+		SetState (State.Initialize);
+
+	}
+
+	IEnumerator Start() {
+		while (true) {
+			switch (layerState) {
+			case State.None:
+				break;
+			case State.Initialize:
+				Initialize();
+				break;
+			case State.Upcoming:
+				Upcoming ();
+				break;
+			case State.Current:
+				Current ();
+				break;
+			case State.Fading:
+				Fading ();
+				break;
+			case State.Success:
+				Success ();
+				break;
+			case State.Fail:
+				Fail ();
+				break;
+			case State.Passed:
+				Passed ();
+				break;
+			}
+			yield return null;
+		}
+	}
+
+	void Update() {
+
+
+	}
+
+	void Initialize() {
+		if (_cacheLayerState != layerState) {
+			print ("Initialize");
+			transform.position = new Vector3(0f,0f,20f);
+			_cacheLayerState = layerState;
+			SetState (State.Upcoming);
+		}
+	}
+
+	void Upcoming() {
+		if (_cacheLayerState != layerState) {
+
+			_cacheLayerState = layerState;
+		}
+	}
+
+	void Current() {
+		if (_cacheLayerState != layerState) {
+
+			_cacheLayerState = layerState;
+		}
+	}
+
+	void Fading() {
+		if (_cacheLayerState != layerState) {
+
+			_cacheLayerState = layerState;
+		}
+
+	}
+
+	void Success() {
+		if (_cacheLayerState != layerState) {
+
+			_cacheLayerState = layerState;
+		}
+
+	}
+
+	void Fail() {
+		if (_cacheLayerState != layerState) {
+
+			_cacheLayerState = layerState;
+		}
+
+	}
+
+	void Passed() {
+		if (_cacheLayerState != layerState) {
+
+			_cacheLayerState = layerState;
+		}
+
+	}
+	
+
+
+	// Received by a Broadcast call
+	void NextSpeed() {
+
+	}
+
+	void SetState(State newState) {
+		_prevLayerState = layerState;
+		_cacheLayerState = State.None;
+		layerState = newState;
+	}
+}
+
