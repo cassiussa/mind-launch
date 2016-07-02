64fe6dd050f32562baacc600751f74ff9248b6dc
Cassius John-Adams <cassius.s.adams@gmail.com>
2016-07-01 23:08:51 -0400

    Added the fade-in of the upcoming shape layers

--- Assets/Scripts/LayerFade.cs
+++ Assets/Scripts/LayerFade.cs
@@ -8,14 +8,19 @@ public class LayerFade : MonoBehaviour {
 	public bool fadeOut = false;
 	private float alphaCounter = 1f;
 	public Color colour;
+	public float coloura;
 	public Color colourInvisible;
 	public Color colourVisible;
+	public Camera camera;
+	public float distance;
+	public float fadeAmount;
 
 	public LayerStates layerStates;
 
 	void Awake () {
 
 		layerStates = GetComponent<LayerStates> ();
+		camera = GameObject.Find ("Main Camera").camera;
 		/* 
 		 * First we need to determine how large the array of children
 		 * with Renderers is going to be so that we can update the
@@ -35,49 +40,41 @@ public class LayerFade : MonoBehaviour {
 				Renderer thisRenderer = transformChild.gameObject.GetComponent<Renderer>();	// Cache this iteration's Renderer
 				childRenderers[rendererIterator] = thisRenderer;				// Add it into the array
 				Color col = thisRenderer.material.color;						// Cache the Color
-				var newMat = new Color(col.r, col.g, col.b, 1);			// Create a new Color reference
+				var newMat = new Color(col.r, col.g, col.b, 1);					// Create a new Color reference
 				thisRenderer.material.color = newMat;							// Set the renderer material's colour to the newMat variable reference
 				rendererIterator++;												// Increment the iteration counter
 			}
 		}
-
-		//distanceMarkerStates = gameObject.GetComponent<DistanceMarkerStates> ();
 	}
 
 	void Update () {
+		distance = Vector3.Distance (transform.position, camera.transform.position);
 		ChangeAlpha();
 	}
 	
 	void ChangeAlpha(){
-		//if (gameObject.activeSelf == false) return;
-
-		/*
-		 * Because we're always either adding to the alphaCounter during and after
-		 * fading in, or subtracting from alphaCounter during or after fading out,
-		 * this means it's always processing when active.  Therefore we should
-		 * probably deactivate this script when certain conditions are met.
-		 */
 		if (fadeOut == true) {
-			//_cacheAlphaCounter = alphaCounter;
-			alphaCounter -= Time.deltaTime / global.fadeTime;						// or we're subtracting time
-		
-
+			alphaCounter -= Time.deltaTime / global.fadeTime;							// or we're subtracting time
 
 			if (alphaCounter < 0) {
 				alphaCounter = Mathf.Clamp01 (alphaCounter);
-				layerStates.layerState = LayerStates.State.Fail;				// Totally faded out and no success or failure yet
+				layerStates.layerState = LayerStates.State.Fail;						// Totally faded out and no success or failure yet
 			} else if (alphaCounter != 0 && alphaCounter != 1) {
-				//if (_cacheAlphaCounter < alphaCounter) {
-				//	print ("fading is happening");
-				//}
 				alphaCounter = Mathf.Clamp01 (alphaCounter);							// Clamp the range
 				colour = Color.Lerp (colourInvisible, colourVisible, alphaCounter);		// Lerp between the invisible colour and the fully opaque colour
-			
+				coloura = colour.a;
 				foreach (Renderer childMaterial in childRenderers) {
 					Color newMat = new Color (childMaterial.material.color.r, childMaterial.material.color.g, childMaterial.material.color.b, colour.a);
 					childMaterial.material.color = newMat;								// Assign a standardized material to the circles
 				}
 
+			}
+		} else if(distance > 10) {
+			fadeAmount = (255-((distance-10)*15))/255;
+			foreach (Renderer childMaterial in childRenderers) {
+				//colour.a = fadeAmount;		// Lerp between the invisible colour and the fully opaque colour
+				Color newMat = new Color (childMaterial.material.color.r, childMaterial.material.color.g, childMaterial.material.color.b, fadeAmount);
+				childMaterial.material.color = newMat;								// Assign a standardized material to the circles
 			}
 		}
 	}
